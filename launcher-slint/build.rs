436630ac141792b95baaa3658f26a21072c7cbe6
liko <likoosn@gmail.com>
2026-06-23 19:26:41 +0300

    build(launcher): зависимость discord-rich-presence + env DISCORD_CLIENT_ID

--- launcher-slint/build.rs
+++ launcher-slint/build.rs
@@ -1,4 +1,5 @@
 fn main() {
     println!("cargo:rerun-if-env-changed=LAUNCHER_DEFAULT_API_URL");
+    println!("cargo:rerun-if-env-changed=DISCORD_CLIENT_ID");
     slint_build::compile("ui/app.slint").expect("failed to compile Slint UI");
 }
